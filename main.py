4db4fdec7e82bf40ea6236fb5ea72f1c0409bfc4
EDJINEDJA <automaticall06@gmail.com>
2022-05-16 07:07:04 +0200

    My second change

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 # My first clone 
 
+def batch(list_files,batch):
+    assert batch>0
+    number_files= len (list_files)
+    
+    if number_files % batch ==0:
+        batch_size=number_files/batch
+    ...
+        
+    return batch_size 
+
+        
+    
+    
+    
+    
+
 def main():
+    
     return None 
 
 if __name__="__main__":
